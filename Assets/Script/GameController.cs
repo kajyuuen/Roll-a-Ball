98a06658333b1d2d52830ae315956ff086fe2e7b
kajyuuen <kajyuuen@gmail.com>
2017-08-11 15:20:32 +0900

    6.ゲームのクリアを追加する

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
 	public void Update ()
 	{
 		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
 		scoreLabel.text = count.ToString ();
+
+		if (count == 0) {
+			// オブジェクトをアクティブにする
+			winnerLabelObject.SetActive (true);
+		}
 	}
 }
